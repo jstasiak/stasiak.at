ef2e24a6e24dfcea13cc8ddee3f393c315b44bb3
Jakub Stasiak <jakub@stasiak.at>
2019-10-14 22:33:52 +0200

    Create a base Pelican structure in preparation for static blog generation

---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Jakub Stasiak'
+SITENAME = 'Jakub Stasiak'
+SITEURL = 'https://stasiak.at'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Warsaw'
+
+DEFAULT_LANG = 'en'
+
+THEME = 'stasiak.at-theme'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = False
+
+STATIC_PATHS = ['jakub@stasiak.at.asc']
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
